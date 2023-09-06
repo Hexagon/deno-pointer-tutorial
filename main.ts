31b223e02fe5d809f9d9ea785a5992a4e85793c6
Hexagon <hexagon@56k.guru>
2023-09-06 23:08:33 +0200

    Fixes

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// Imports
 import {
   Application,
   Router,
@@ -7,21 +8,26 @@ import {
 import { exists } from "https://deno.land/std@0.192.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.192.0/path/mod.ts";
 
+// Constants
 const LISTEN_PORT = 19192;
 const STATIC_DIR = "./static";
 const ROOT_PATH = "/";
+const BROADCAST_INTERVAL = 150; // milliseconds
 
+// Initialize Oak Application and Router
 const app = new Application();
 const router = new Router();
 
+// Define Client Interface
 interface Client {
   socket: WebSocket;
   pos: { x: number; y: number; id: string };
 }
 
+// Active clients list
 const clients: Client[] = [];
 
-// Remove a client from the clients array
+// Helper Functions
 const removeClient = (client: Client) => {
   const index = clients.indexOf(client);
   if (index > -1) {
@@ -29,97 +35,74 @@ const removeClient = (client: Client) => {
   }
 };
 
-// Send positions of all clients to all clients
 const broadcastPositions = () => {
   const positions = clients.map((client) => client.pos);
-  for (const client of clients) {
-    const otherPositions = positions.filter((pos) => pos.id !== client.pos.id); // Filter out own position
-    client.socket.send(JSON.stringify(otherPositions));
-  }
+  clients.forEach((client) => {
+    const filteredPositions = positions.filter((pos) =>
+      pos.id !== client.pos.id
+    );
+    client.socket.send(JSON.stringify(filteredPositions));
+  });
 };
 
-// Handle WebSocket Connection
 const handleWebSocketConnection = (ws: WebSocket) => {
-  const client: Client = {
-    socket: ws,
-    pos: { x: 0, y: 0, id: "undef" },
-  };
+  const client: Client = { socket: ws, pos: { x: 0, y: 0, id: "undef" } };
 
-  ws.onopen = () => {
-    clients.push(client);
-  };
-
+  ws.onopen = () => clients.push(client);
   ws.onmessage = (event) => {
-    const newPos = JSON.parse(event.data);
-    client.pos.x = parseInt(newPos.x, 10);
-    client.pos.y = parseInt(newPos.y, 10);
-    client.pos.id = newPos.id.substring(0, 10); // Allow at most 10 chars
+    const { x, y, id } = JSON.parse(event.data);
+    client.pos = {
+      x: parseInt(x, 10),
+      y: parseInt(y, 10),
+      id: id.substring(0, 10),
+    };
   };
-
-  ws.onclose = () => {
-    removeClient(client);
-  };
-
+  ws.onclose = () => removeClient(client);
   ws.onerror = () => {
     console.error("WebSocket error observed");
     removeClient(client);
   };
 };
 
+// Routing
 router.get("/ws", (ctx) => {
   if (!ctx.isUpgradable) {
     ctx.throw(501);
     return;
   }
-  try {
-    const ws = ctx.upgrade();
-    handleWebSocketConnection(ws);
-  } catch (err) {
-    console.error("Failed to upgrade the connection", err);
-    ctx.throw(500);
-  }
+  const ws = ctx.upgrade();
+  handleWebSocketConnection(ws);
 });
 
-router.get("/", ({ response }: { response: any }) => {
+router.get("/", ({ response }) => {
   response.redirect("/index.html");
 });
 
+// Middleware
 app.use(router.routes())
   .use(router.allowedMethods())
   .use(async (ctx, next) => {
-    if (!ctx.request.url.pathname.startsWith(ROOT_PATH)) {
-      await next();
-      return;
-    }
-
     const filePath = ctx.request.url.pathname.replace(ROOT_PATH, "");
+    const localFilePath = await join(STATIC_DIR, filePath);
 
-    const localFilePath = await join(
-      STATIC_DIR,
-      ctx.request.url.pathname.replace(ROOT_PATH, ""),
-    );
-
-    const fExists = await exists(localFilePath, {
-      isFile: true,
-      isReadable: true,
-    });
-    if (fExists) {
+    if (await exists(localFilePath)) {
       await send(ctx, filePath, { root: STATIC_DIR });
     } else {
       await next();
     }
   })
   .use((ctx) => {
-    // If no route has been matched
     ctx.response.status = Status.NotFound;
     ctx.response.body = "Not Found";
   });
 
-// Start an interval for broadcasing mouse positions
-const broadcastInterval = setInterval(broadcastPositions, 150);
-// but dont keep the main loop running just because of this
+// Run Server
+const broadcastInterval = setInterval(broadcastPositions, BROADCAST_INTERVAL);
 Deno.unrefTimer(broadcastInterval);
 
 app.listen({ port: LISTEN_PORT });
-
-console.log("Started");
+console.log(`Server started at http://localhost:${LISTEN_PORT}`);
+console.log(
+  `Visit http://localhost:${LISTEN_PORT}/index.html for the main page.`,
+);
+console.log(`WebSocket endpoint is ws://localhost:${LISTEN_PORT}/ws`);
